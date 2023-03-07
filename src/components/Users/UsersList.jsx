0e3bf81058ff14598c15640a9ac5caffbb2bc2c1
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-03-07 16:36:45 -0500

    fix

--- src/components/Users/UsersList.jsx
+++ src/components/Users/UsersList.jsx
@@ -3,17 +3,15 @@ import React from "react";
 import Card from "../UI/Card";
 import classes from "./UsersList.module.scss";
 
-const UsersList = (usersProp) => {
+const UsersList = ({ usersProp }) => {
 	return (
 		<Card className={classes.users}>
 			<ul>
-				{usersProp.map((user) => {
-					return (
-						<li>
-							{user.name} ({user.age} years old)
-						</li>
-					);
-				})}
+				{usersProp.map((user) => (
+					<li key={user.id}>
+						{user.name} ({user.age} years old)
+					</li>
+				))}
 			</ul>
 		</Card>
 	);

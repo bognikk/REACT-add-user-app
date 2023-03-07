8b43cbfd1a8da9d3dc05adff31d32d2ebbac192a
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-03-07 15:34:12 -0500

    users list comp

---
+++ src/components/Users/UsersList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Card from "../UI/Card";
+import classes from "./UsersList.module.scss";
+
+const UsersList = ({ usersProp }) => {
+	return (
+		<Card className={classes.users}>
+			<ul>
+				{usersProp.map((user) => {
+					<li>
+						{user.name} ({user.age} years old)
+					</li>;
+				})}
+			</ul>
+		</Card>
+	);
+};
+
+export default UsersList;
